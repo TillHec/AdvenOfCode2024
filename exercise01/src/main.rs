467fd2cc0a912dac9a74792ed1bcaa20db6aec69
Till Heckelbacher <till.heckelbacher@posteo.de>
2024-12-22 22:49:50 +0100

    Exercise 01: Solution to part 2

--- exercise01/src/main.rs
+++ exercise01/src/main.rs
@@ -1,11 +1,16 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
     let content = FileContent::from("input.txt");
     let mut locations = Locations::from(content.content_as_vec());
     locations.sort();
-    let result = locations.distance().iter().fold(0, |acc, x| acc + x);
-    print!("The answer is {}\n", result);
+    let result1 = locations.distance().iter().fold(0, |acc, x| acc + x);
+    print!("The answer to exercise 1 is {}\n", result1);
+    locations.set_occurrences_left_and_initialize_right();
+    locations.set_occurrences_right();
+
+    println!("The answer to exercise 2 is {}\n", locations.get_similarity_score());
 }
 
 struct FileContent {
@@ -27,6 +32,8 @@ impl FileContent {
 struct Locations {
     left_side: Vec<i32>,
     right_side: Vec<i32>,
+    occurrences_left: HashMap<i32, usize>,
+    occurrences_right: HashMap<i32, usize>,
 }
 
 fn split_string_to_integers(string: &String, index: usize) -> i32 {
@@ -38,7 +45,7 @@ impl Locations {
     fn from(file_content: Vec<String>) -> Locations {
         let left_side: Vec<i32> = file_content.iter().map(|line| split_string_to_integers(line, 0)).collect();
         let right_side: Vec<i32> = file_content.iter().map(|line| split_string_to_integers(line, 1)).collect();
-        Locations {left_side, right_side}
+        Locations {left_side, right_side, occurrences_left: HashMap::new(), occurrences_right: HashMap::new()}
     }
 
     fn sort(&mut self) {
@@ -52,5 +59,33 @@ impl Locations {
             .zip(self.right_side.iter())
             .map(|(left_side, right_side)| (left_side - right_side).abs() as u32)
             .collect()
+    }
+
+    fn set_occurrences_left_and_initialize_right(&mut self) {
+        for number in self.left_side.iter() {
+            self.occurrences_right.entry(*number).or_insert(0);
+
+            let count = self.occurrences_left.entry(*number).or_insert(0);
+            *count += 1;
+        }
+    }
+
+    fn set_occurrences_right(&mut self) {
+        for number in self.right_side.iter() {
+            let count = self.occurrences_right.entry(*number).or_insert(0);
+            *count += 1;
+        }
+    }
+
+    fn get_similarity_score(&self) -> usize {
+        let mut score = 0;
+        self.occurrences_left
+            .keys()
+            .for_each(
+                |key| {
+                    score += key.abs() as usize * self.occurrences_left.get(key).unwrap() * self.occurrences_right.get(key).unwrap()
+                }
+            );
+        score
     }
 }
